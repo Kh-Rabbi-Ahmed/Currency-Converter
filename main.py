7f6387237b17727532056d43e9d863a9855b6a1a
Khandakar Rabbi Ahmed <89539425+Kh-Rabbi-Ahmed@users.noreply.github.com>
2022-04-21 15:22:31 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+taka= input("Enter Your Total Money=")
+intTaka = int(taka)
+dollar,rupi = (intTaka/ 86), (intTaka*0.88323)
+
+print(intTaka, "taka= $ ", dollar)
+print(intTaka, "taka= rupi ", rupi)
